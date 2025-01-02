1fd5997327d3bf0a346df7e5446c0522855e6a37
KasKes <info@camposviola.de>
2025-01-02 19:11:32 +0100

    no index

--- .astro/astro/content.d.ts
+++ .astro/astro/content.d.ts
@@ -1088,7 +1088,7 @@ declare module 'astro:content' {
   slug: "-index";
   body: string;
   collection: "homepage";
-  data: InferEntrySchema<"homepage">
+  data: any
 } & { render(): Render[".mdx"] };
 };
 "igotshot": {
@@ -1858,35 +1858,28 @@ declare module 'astro:content' {
   slug: "datenschutz";
   body: string;
   collection: "pages";
-  data: InferEntrySchema<"pages">
-} & { render(): Render[".mdx"] };
-"elements.mdx": {
-	id: "elements.mdx";
-  slug: "elements";
-  body: string;
-  collection: "pages";
-  data: InferEntrySchema<"pages">
+  data: any
 } & { render(): Render[".mdx"] };
 "privacy-policy.md": {
 	id: "privacy-policy.md";
   slug: "privacy-policy";
   body: string;
   collection: "pages";
-  data: InferEntrySchema<"pages">
+  data: any
 } & { render(): Render[".md"] };
 "sitemap.mdx": {
 	id: "sitemap.mdx";
   slug: "sitemap";
   body: string;
   collection: "pages";
-  data: InferEntrySchema<"pages">
+  data: any
 } & { render(): Render[".mdx"] };
 "w50jahre.mdx": {
 	id: "w50jahre.mdx";
   slug: "w50jahre";
   body: string;
   collection: "pages";
-  data: InferEntrySchema<"pages">
+  data: any
 } & { render(): Render[".mdx"] };
 };
 "preise": {
@@ -2260,5 +2253,5 @@ declare module 'astro:content' {
 
 	type AnyEntryMap = ContentEntryMap & DataEntryMap;
 
-	export type ContentConfig = typeof import("../../src/content/config.js");
+	export type ContentConfig = never;
 }
